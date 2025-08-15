87450bb1d8e26b4a4cdf6b7941781e43a27939b8
Wink Saville <wink@saville.com>
2025-08-15 11:58:34 -0700

    feat: Add the ability to make a hole
    It's always in the center of the cuboid and by default there is no hole.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use clap::{CommandFactory, Parser};
-use csgrs::float_types::Real;
+use clap::{CommandFactory, Parser, value_parser};
+use csgrs::{float_types::Real, traits::CSG};
 use nalgebra::Vector3;
 
 type Mesh = csgrs::mesh::Mesh<()>;
@@ -13,7 +13,7 @@ struct Args {
     width: Real,
 
     /// length of the panel
-    #[arg(short, long, default_value = "100.0")]
+    #[arg(short, long, default_value = "20.0")]
     length: Real,
 
     /// height of the panel
@@ -21,12 +21,12 @@ struct Args {
     height: Real,
 
     /// hole diameter, zero for no hole
-    #[arg(short = 'd', long, default_value = "0")]
+    #[arg(short, long, default_value = "0")]
     diameter: Real,
 
-    /// hole support thickness
-    #[arg(short = 's', long, default_value = "0")]
-    support_thickness: Real,
+    /// number of segments to use when creating the tube, minimum is 3
+    #[arg(short, long, default_value = "50", value_parser = value_parser!(u32).range(3..), help = "The number of segments to use when creating the tube, minimum is 3")]
+    segments: u32,
 }
 
 fn main() {
@@ -39,25 +39,52 @@ fn main() {
         clap_metadata.get_version().unwrap_or_default()
     );
 
-    let outside: Mesh;
-
     // Simple code
     let cuboid_dimensions = Vector3::new(args.width, args.length, args.height);
     println!("outside_dimenstions: {:?}", cuboid_dimensions);
-    outside = Mesh::cuboid(
+    let mut cuboid = Mesh::cuboid(
         cuboid_dimensions.x,
         cuboid_dimensions.y,
         cuboid_dimensions.z,
         None,
     );
 
+    // Someday union a hole support cylinder with the cubiod if requested
+    // this doesn't work because I can't get prusa-clicer to do 100% infill
+    // for the hole support cylinder. The workaround is to probably use
+    // a modifier cylinder in prusa-slicer and indicating 100% infill
+
+    let mut hole_diameter_file_name = String::new();
+    // Create a hole in the cuboid if diameter is greater than zero
+    if args.diameter > 0.0 {
+        let hole_diameter = args.diameter;
+        let hole_radius = hole_diameter / 2.0;
+
+        let center_location = Vector3::new(
+            cuboid_dimensions.x / 2.0,
+            cuboid_dimensions.y / 2.0,
+            cuboid_dimensions.z / 2.0,
+        );
+
+        let hole = Mesh::cylinder(
+            hole_radius,
+            args.height,
+            args.segments as usize,
+            None,
+        ).translate(center_location.x, center_location.y, 0.0);
+
+        cuboid = cuboid.difference(&hole);
+
+        hole_diameter_file_name = format!("_d-{:.2}", args.diameter);
+    }
 
     // Write the result as an ASCII STL:
     let name = "holed-cuboid".to_owned()
         + &format!("_w-{:.2}", args.width)
         + &format!("_l-{:.2}", args.length)
-        + &format!("_h-{:.2}", args.height);
-    let stl = outside.to_stl_ascii(&name);
+        + &format!("_h-{:.2}", args.height)
+        + &hole_diameter_file_name;
+    let stl = cuboid.to_stl_ascii(&name);
     let file_name = name + ".stl";
     println!("Writing STL file: {}", file_name);
     std::fs::write(file_name, stl).unwrap();
